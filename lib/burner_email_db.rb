8816b9fb1af78e638f938905b57950294ff33287
Sergey Pedan <sergey.pedan@gmail.com>
2021-02-07 15:09:06 +0700

    Writes tests

--- lib/burner_email_db.rb
+++ lib/burner_email_db.rb
@@ -3,6 +3,11 @@
 require_relative "burner_email_db/version"
 
 module BurnerEmailDb
-  class Error < StandardError; end
-  # Your code goes here...
+
+  FILE_PATH = File.expand_path("../burner_email_db/db.txt", __FILE__).freeze
+
+  def self.domains
+    File.readlines(FILE_PATH).map(&:chomp)
+  end
+
 end
--- spec/burner_email_db_spec.rb
+++ spec/burner_email_db_spec.rb
@@ -5,7 +5,35 @@ RSpec.describe BurnerEmailDb do
     expect(BurnerEmailDb::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".domains" do
+    subject { described_class.domains }
+    let(:random_record) { subject.sample }
+
+    it "returns an Array" do
+      expect(subject).to be_an Array
+    end
+
+    it "contains multiple records" do
+      expect(subject.size).to be > 1
+    end
+
+    it "records are strings" do
+      subject.each do |record|
+        expect(record).to be_a String
+      end
+    end
+
+    it "records have no new line symbols at the end" do
+      subject.each do |record|
+        expect(record).not_to include "\n"
+      end
+    end
+
+    it "records have white spaces" do
+      subject.each do |record|
+        expect(record).not_to include "\s"
+      end
+    end
+
   end
 end
